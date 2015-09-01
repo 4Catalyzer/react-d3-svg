9659726e4ad434498cb92c308f26a52c9b173e40
Jimmy Jia <tesrin@gmail.com>
2015-09-01 18:38:42 -0400

    Match real d3 API for line

--- src/Line.js
+++ src/Line.js
@@ -4,39 +4,24 @@ import React from 'react';
 export default class Line extends React.Component {
   static propTypes = {
     data: React.PropTypes.array.isRequired,
-    xScale: React.PropTypes.func,
-    yScale: React.PropTypes.func,
-    xKey: React.PropTypes.any.isRequired,
-    yKey: React.PropTypes.any.isRequired,
+    x: React.PropTypes.func,
+    y: React.PropTypes.func,
     defined: React.PropTypes.func,
     interpolate: React.PropTypes.string,
     tension: React.PropTypes.string
   };
 
-  static contextTypes = {
-    xScale: React.PropTypes.func,
-    yScale: React.PropTypes.func
-  };
-
-  static defaultProps = {
-    xKey: 0,
-    yKey: 1
-  };
-
   render() {
-    const {
-      data, xKey, yKey,
-      defined, interpolate, tension,
-      ...props
-    } = this.props;
+    const {data, x, y, defined, interpolate, tension, ...props} = this.props;
 
-    const xScale = this.props.xScale || this.context.xScale;
-    const yScale = this.props.yScale || this.context.yScale;
+    const line = d3.svg.line();
 
-    const line = d3.svg.line()
-      .x(d => xScale(d[xKey]))
-      .y(d => yScale(d[yKey]));
-
+    if (x) {
+      line.x(x);
+    }
+    if (y) {
+      line.y(y);
+    }
     if (defined) {
       line.defined(defined);
     }
